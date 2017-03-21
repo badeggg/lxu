26beb39be32b55b34b6bc1fb2d6585448cba829c
badeggg <zhaoxuxujc@163.com>
2017-03-21 18:57:40 +0800

    in progress...

---
+++ src/proto.js
@@ -0,0 +1,17 @@
+let properties = {
+  use: {
+    /**
+     * Parameters pattern:
+     * ([method,][path,]fn) or object-argument
+     */
+    value: function(method, path, fn){
+      if
+    },
+  },
+  middlewares: {
+    value: [],
+  },
+
+};
+
+let proto = Object.defineProperties({}, properties);
