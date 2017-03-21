dc38772d12f0e0cee8f754bcb519cc813f854a3c
badeggg <zhaoxuxujc@163.com>
2017-03-21 22:57:24 +0800

    In progress...

--- src/proto.js
+++ src/proto.js
@@ -12,13 +12,13 @@ let properties = {
      */
     value: function(method, path, fn){
       isPureObject(arguments[0])
-        ?(method = arguments[0]['method'] || 'GET',
-          path = arguments[0]['path'] || '/',
-          fn = arguments[0]['fn'])
-        :(fn = arguments[arguments.length-1]
-          arguments.length === 3 && (path = arguments[1] || '/') && (method = argumnets[0] || 'GET'),
-          arguments.length === 2 && (isHttpMethod(arguments[0]) ? method = arguments[0]: path = arguments[0]) || '/'
-          );
+      ?(method = arguments[0]['method'] || 'GET',
+        path = arguments[0]['path'] || '/',
+        fn = arguments[0]['fn'])
+      :(fn = arguments[arguments.length-1]
+        arguments.length === 3 && (path = arguments[1] || '/') && (method = argumnets[0] || 'GET'),
+        arguments.length === 2 && (isHttpMethod(arguments[0]) ? method = arguments[0]: path = arguments[0]) || '/'
+        );
 
       if( !isPlainFunction(fn) && !isAsyncFunction(fn) ){
         throw new Error('app.use expect a middleware which should be a plain function or an async function.');
@@ -31,16 +31,13 @@ let properties = {
     },
   },
   consume: {
-    //this part is wrong!!!
     value: function(req, res){
       let fn = this.middlewares.shift();
-      while(this.middlewares.length >= 1){
+      function next(){
         let nextFn = this.middlewares.shift();
-        function next(){
-          nextFn(req, res, next);
-        }
-        fn(req, res, next);
+        nextFn && nextFn(req, res, next);
       }
+      fn(req, res, next);
     }
   },
   middlewares: {
