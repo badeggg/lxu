66fcf4cc1a9881ef6c1b551a5556186dd594be9b
badeggg <zhaoxuxujc@163.com>
2017-03-31 11:35:16 +0800

    In progress...

--- test/src/component/isMiddlewareMatch_test.js
+++ test/src/component/isMiddlewareMatch_test.js
@@ -7,31 +7,36 @@ const assert = require('assert');
 const isMiddlewareMatch = require('../../../src/component/isMiddlewareMatch.js');
 
 let mv1 = ['get', '/', ()=>{}];
-let mv2 = ['post', '/test/path', ()=>{}];
+let mv2 = ['post', 'test/path', ()=>{}];
 let mv3 = ['Get', '/test/path/', ()=>{}];
 let mv4 = ['POST', 'test/path', ()=>{}];
 
-let server = _testConnect.server((req, res)=>{
+let server1 = _testConnect.server((req, res)=>{
   assert(isMiddlewareMatch(mv1, req) === true);
-  server.destroy();
+  server1.destroy();
 }, 9091);
 _testConnect.client({method: 'get', path: '/', port: 9091});
 
-server = _testConnect.server((req, res)=>{
-  //console.log(isMiddlewareMatch(mv2, req));
-  //assert(isMiddlewareMatch(mv2, req) === true);
-  server.destroy();
+// let server2 = _testConnect.server((req, res)=>{
+//   assert(isMiddlewareMatch(mv1, req) === true);
+//   server2.destroy();
+// }, 9092);
+// _testConnect.client({method: 'get', path: '/', port: 9092});
+
+let server2 = _testConnect.server((req, res)=>{
+  assert(isMiddlewareMatch(mv2, req) === true);
+  server2.destroy();
 }, 9092);
 _testConnect.client({method: 'post', path: '/test/path', port: 9092});
 
-// server = _testConnect.server((req, res)=>{
-//   assert(isMiddlewareMatch(mv3, req) === true);
-//   server.destroy();
-// }, 9093);
-// _testConnect.client({method: 'get', path: 'test/path', port: 9093});
+let server3 = _testConnect.server((req, res)=>{
+  //assert(isMiddlewareMatch(mv3, req) === true);
+  server3.destroy();
+}, 9093);
+_testConnect.client({method: 'get', path: '/test/path/', port: 9093});
 
-// server = _testConnect.server((req, res)=>{
-//   assert(isMiddlewareMatch(mv4, req) === true);
-//   server.destroy();
+// let server4 = _testConnect.server((req, res)=>{
+//   //assert(isMiddlewareMatch(mv4, req) === true);
+//   server4.destroy();
 // }, 9094);
 // _testConnect.client({method: 'post', path: '/test/path/', port: 9094});
